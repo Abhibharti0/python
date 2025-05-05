01aff8946e8a41fa475b65739950e917c02ea970
Abhishek Bharti <abhibharti457@gmail.com>
2025-05-05 21:19:17 +0530

    Add files via upload

---
+++ day16/oopsinpython.py
@@ -0,0 +1,28 @@
+# import turtle
+# tinny=turtle.Turtle() #new object where Turtle is class
+# print(type(tinny))
+
+
+"""from turtle import Turtle, Screen
+tinny=Turtle()
+print(tinny)
+tinny.shape("turtle")
+tinny.color("chartreuse4")
+tinny.forward(100)
+tinny.left(120)
+tinny.forward(100)
+tinny.left(120)
+tinny.forward(100)
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()"""
+
+
+
+"""table creation using prettytable module """
+# import prettytable
+# abhi=prettytable.PrettyTable()
+# abhi.add_column("pokemon Name",["pikachu","Squirtle","Charmander"])
+# abhi.add_column("Type",["Electri","Water","fire"])
+# abhi.align="l"
+# print(abhi)
